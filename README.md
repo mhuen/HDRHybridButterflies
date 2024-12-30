4d2464e018e7138b7b776c0c8918d3c5a3c07fb9
mhuen <mhuen@users.noreply.github.com>
2024-12-30 08:22:36 -0600

    initial repository

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Unit Tests](https://github.com/mhuen/HDRHybridButterflies/actions/workflows/test_suite.yml/badge.svg)](https://github.com/mhuen/HDRHybridButterflies/actions/workflows/test_suite.yml)
+[![codecov](https://codecov.io/gh/mhuen/HDRHybridButterflies/graph/badge.svg?token=WUUXF6JCHG)](https://codecov.io/gh/mhuen/HDRHybridButterflies)
+
+
 # HDRHybridButterflies
 Solution for HDR Hybrid Butterflies CodaBench Challenge
---
+++ hdr_hybrid_butterflies/__about__.py
@@ -0,0 +1,15 @@
+__version_major__ = 0
+__version_minor__ = 0
+__version_patch__ = 0
+__version_info__ = "-dev"
+
+__version__ = "{}.{}.{}{}".format(
+    __version_major__,
+    __version_minor__,
+    __version_patch__,
+    __version_info__,
+)
+__author__ = "Mirco Huennefeld"
+__author_email__ = "mirco.huennefeld@tu-dortmund.de"
+__description__ = "HDR Hybrid Butterfly Solver"
+__url__ = "https://github.com/mhuen/HDRHybridButterflies"
