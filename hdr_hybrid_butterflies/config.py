a5263a04586040f5edd55d22becd84e664d18c2e
mhuen <mhuen@users.noreply.github.com>
2025-01-21 17:11:50 -0600

    Use v01 for segmentation model

--- hdr_hybrid_butterflies/config.py
+++ hdr_hybrid_butterflies/config.py
@@ -15,3 +15,19 @@ CNN_SEGMENTER_CONFIG_V01 = {
     "use_residual_list": True,
     "method_list": "convolution",
 }
+
+
+CNN_SEGMENTER_CONFIG_V02 = {
+    "filter_size_list": [[5, 5]] * 80,
+    "num_filters_list": [8] * 80,
+    "pooling_type_list": None,
+    "pooling_strides_list": [[1, 2, 2, 1]] * 80,
+    "pooling_ksize_list": [[1, 2, 2, 1]] * 80,
+    "strides_list": [1, 1, 1, 1],
+    "dilation_rate_list": [[1, 1, 1, 1], [1, 1, 1, 1]] * 40,
+    "use_dropout_list": False,
+    "use_batch_normalisation_list": False,
+    "activation_list": "elu",
+    "use_residual_list": True,
+    "method_list": "convolution",
+}
